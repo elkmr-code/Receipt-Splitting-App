660f3abd1aa3c326b39abe4d01e3eeae1385015f
elkmr-code <whc2023work@gmail.com>
2025-08-04 22:29:29 +0800

    Initial Commit

---
+++ Grocery Split AppTests/Grocery_Split_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Grocery_Split_AppTests.swift
+//  Grocery Split AppTests
+//
+//  Created by user12 on 2025/8/4.
+//
+
+import Testing
+@testable import Grocery_Split_App
+
+struct Grocery_Split_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
